a1f513b9530c83b14c5c01e97c057af99bfc6342
Taz Campbell <shelbyacampbell2724@gmail.com>
2024-02-07 21:09:06 -0500

    roger roger

--- server.js
+++ server.js
@@ -2,14 +2,14 @@ require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors')
-const PeopleRouter = require('./controllers/People') //new
+const PeopleRouter = require('./controllers/people') //new
 
 const app = express();
 
 //middleware
 app.use(cors())
 app.use(morgan('dev'))
-app.use(express.json()) //this middleware allows us to send JSON request with our request
+app.use(express.json()) //this middle ware allows us to send JSON request with our request
 
 //routes
 app.use('/people', PeopleRouter);
